4aa3d3c4e50ccc3acb318ed7ff538fe077f5346c
Kammi Henderson <kammi_henry@yahoo.com>
2024-06-03 15:36:34 -0500

    Status button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const statusButton = document.querySelector("button");
+const statusButton = document.querySelector(
+   "button"
+);
 const pets = document.querySelector(".all-pets");
 
 const createPet = function (name, species) {
@@ -7,19 +9,22 @@ const createPet = function (name, species) {
         species: species,
         isTired: 5,
         sleep: function () {
-          console.log(`${this.name} needs nap. Zzz...`);
-          this.isTired = 1;
+          console.log(
+            `${this.name} needs nap. Zzz...`
+        );
+        this.isTired = 1;
         },
         play: function () {
             if (this.isTired === 10) {
                 console.log("Too tired to play.");
                 this.sleep();
             } else {
-                console.log(`Yay! ${this.name} loves to play!`);
+                console.log(`Yay! ${this.name} loves to play!`
+                );
                 this.isTired += 1;
             }
-        }
-    };
+          }
+       }; 
 
     return pet;
 };
@@ -30,9 +35,44 @@ const baxter = createPet("Baxter", "hampster");
 const cleo = createPet = ("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
 
-console.log(sora, clover, baxter, cleo, francine);
+// console.log(sora, clover, baxter, cleo, francine);
 
-clover.sleep();
-baxter.play();
+// clover.sleep();
+// baxter.play();
 
-console.log(clover, baxter);
+// console.log(clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [
+    sora,
+    clover,
+    baxter,
+    cleo,
+    francine,
+];
+
+// console.log(allPets);
+
+const showPets = function (petArray) {
+    // empty list
+    pets.innerHTML = "";
+
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7) {
+          status = "sleeping.";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+    }
+};
+
+statusButton.addEventListener(
+    "click",
+    function () {
+      showPets(allPets);
+    }
+);
